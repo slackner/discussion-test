15dcbba2507a0984fd30ca88bfd58ca07a0d14b3
Michael Müller <michael.mueller@sysmagine.com>
2023-10-09 00:54:20 +0200

    Move calc_hash out of print_hashes.

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
 import os
 import hashlib
 
+def calc_hash(file_path):
+    h = hashlib.sha256()
+    with open(file_path, "rb") as fp:
+        while True:
+            data = fp.read(4096)
+            if data == b"":
+                break
+            h.update(data)
+
+    return h
+
 def print_hashes(path):
     for file in os.listdir(path):
         file_path = os.path.join(path, file)
@@ -8,17 +19,6 @@ def print_hashes(path):
             continue
         if not os.path.isfile(file_path):
             continue
-
-        def calc_hash(file_path):
-            h = hashlib.md5()
-            with open(file_path, "rb") as fp:
-                while True:
-                    data = fp.read(4096)
-                    if not data:
-                        break
-                    h.update(data)
-
-            return h
 
         h = calc_hash(file_path)
         print (f"{h.hexdigest()} {file}")
